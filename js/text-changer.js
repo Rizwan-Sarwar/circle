320d60343e74d0d9fba54019ed697fbe362b45d5
Rizwan Sarwar <71431169+Rizwan-Sarwar@users.noreply.github.com>
2020-12-14 01:18:07 +0500

    Add files via upload

---
+++ js/text-changer.js
@@ -0,0 +1,20 @@
+function design(){
+    document.getElementById("heading").innerHTML = "design";
+    document.getElementById("peragraph").innerHTML = "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum'.";
+}
+function sourcing(){
+    document.getElementById("heading").innerHTML = "sourcing";
+    document.getElementById("peragraph").innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,";
+}
+function production(){
+    document.getElementById("heading").innerHTML = "production";
+    document.getElementById("peragraph").innerHTML = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.";
+}
+function logistic(){
+    document.getElementById("heading").innerHTML = "logistic";
+    document.getElementById("peragraph").innerHTML = "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus";
+}
+function retail(){
+    document.getElementById("heading").innerHTML = "retail";
+    document.getElementById("peragraph").innerHTML = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.";
+}
